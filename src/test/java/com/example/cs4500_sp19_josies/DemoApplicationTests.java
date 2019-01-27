d919735121d7cf27c750326529815a46120c5b82
Mike Sarfaty <msarfaty@wanderu.com>
2019-01-27 14:55:21 -0500

    init commit

---
+++ src/test/java/com/example/cs4500_sp19_josies/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.cs4500_sp19_josies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
