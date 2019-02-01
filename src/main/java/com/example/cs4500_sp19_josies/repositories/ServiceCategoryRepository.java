2cdd7c8deaa40317df6fd2767edb074dc40fc9c0
Khyati Singh <singh.khy@husky.neu.edu>
2019-02-01 16:55:19 -0500

    Delete repo

--- src/main/java/com/example/cs4500_sp19_josies/repositories/ServiceCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cs4500_sp19_josies.repositories;
-
-import com.example.cs4500_sp19_josies.models.ServiceCategory;
-import org.springframework.data.repository.*;
-
-public interface ServiceCategoryRepository
-  extends CrudRepository<ServiceCategory, Integer> {}
