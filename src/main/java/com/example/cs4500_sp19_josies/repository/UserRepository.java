bf1d41c514afd518315349da771babe0b29478af
dixon <jin.di@husky.neu.edu>
2019-02-01 17:57:03 -0500

    Created user repository and inserted users

---
+++ src/main/java/com/example/cs4500_sp19_josies/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.cs4500_sp19_josies.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.cs4500_sp19_josies.models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+  @Query(value="SELECT user FROM User user")
+  public List<User> findAllUsers();
+  @Query(value="SELECT user FROM User user WHERE user.id=:id")
+  public User findUserById(@Param("id") Integer id);
+  @Query(value="SELECT user FROM User user WHERE user.username=:username")
+  public User findByUsername(@Param("username") String username);
+}
