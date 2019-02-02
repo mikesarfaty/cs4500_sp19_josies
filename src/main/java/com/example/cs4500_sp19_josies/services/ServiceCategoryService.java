e531a6ed1838edccfe1b1f3871683979e365f3e5
Nii Ako Boi-Doku <niiakoboi-doku@Niis-MacBook-Pro.local>
2019-02-01 21:28:22 -0500

    Added Service Category Service

---
+++ src/main/java/com/example/cs4500_sp19_josies/services/ServiceCategoryService.java
@@ -0,0 +1,56 @@
+package com.example.cs4500_sp19_josies.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cs4500_sp19_josies.models.ServiceCategory;
+import com.example.cs4500_sp19_josies.repositories.ServiceCategoryRepository;
+
+
+
+@RestController
+public class ServiceCategoryService {
+
+    @Autowired
+    ServiceCategoryRepository serviceRepository;
+
+    @GetMapping("/api/categories")
+    public List<ServiceCategory> findAllServiceCategories() {
+
+        return serviceRepository.findAllServiceCategories();
+    }
+
+    @GetMapping("/api/categories/{serviceCategoryId}")
+    public ServiceCategory findServiceCategoryById(
+            @PathVariable("serviceCategoryId") Integer id) {
+        return serviceRepository.findServiceCategoryById(id);
+    }
+
+    @PostMapping("/api/categories")
+    public ServiceCategory createServiceCategory(@RequestBody ServiceCategory serviceCategory) {
+        return serviceRepository.save(serviceCategory);
+    }
+
+    @PutMapping("/api/categories/{serviceCategoryId}")
+    public ServiceCategory updateServiceCategory(
+            @PathVariable("serviceCategoryId") Integer id,
+            @RequestBody ServiceCategory serviceUpdates) {
+        ServiceCategory serviceCategory = serviceRepository.findServiceCategoryById(id);
+        serviceCategory.setServiceCategoryName(serviceUpdates.getServiceCategoryName());
+        return serviceRepository.save(serviceCategory);
+    }
+    
+    @DeleteMapping("/api/categories/{serviceCategoryId}")
+    public void deleteServiceCategory(
+            @PathVariable("serviceCategoryId") Integer id) {
+        serviceRepository.deleteById(id);
+    }
+}
