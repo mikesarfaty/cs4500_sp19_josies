ecb431edfe88e34b6bfd738cb952c96f5f30e567
jeffchampion <champion.j@husky.neu.edu>
2019-02-08 16:47:56 -0500

    started faqservice restful

---
+++ src/main/java/com/example/cs4500_sp19_josies/services/FAQService.java
@@ -0,0 +1,28 @@
+package com.example.cs4500_sp19_josies.services;
+
+import com.example.cs4500_sp19_josies.models.FrequentlyAskedQuestion;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin(origins="*")
+public class FAQService {
+  @Autowired
+  FAQRepository repository;
+  @GetMapping("/api/faqs")
+  public List<FrequentlyAskedQuestion> findAllFrequentlyAskedQuestions() {
+    return repository.findAllFrequentlyAskedQuestions();
+  }
+  @GetMapping("/api/faqs/{id}")
+  public FrequentlyAskedQuestion findFrequentlyAskedQuestionById(
+          @PathVariable("id") Integer id) {
+    return repository.findFrequentlyAskedQuestionById(id);
+  }
+}
