75555291a4bef6167d7f4bef526a878e4d7c2b40
Daniel Yun <danielyun459@gmail.com>
2019-02-22 16:23:43 -0500

    create_estimate_table.sql added

--- src/main/java/com/example/cs4500_sp19_josies/tests/DeliveryFeeEstimateTest.java
+++ src/main/java/com/example/cs4500_sp19_josies/tests/DeliveryFeeEstimateTest.java
@@ -1,4 +1,16 @@
 package com.example.cs4500_sp19_josies.tests;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class DeliveryFeeEstimateTest {
+
+    @BeforeEach
+//    init if you want to have same scenario .
+    basePrice = 100
+
+
+
+
+
+
 }
