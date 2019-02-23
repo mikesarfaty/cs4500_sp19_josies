9cdd686a437e540dece8186c4ef4f0659521e54b
Nii Ako Boi-Doku <niiakoboi-doku@Niis-MacBook-Pro.local>
2019-02-22 22:37:33 -0500

    Added Unit Test Class for Searching for Services

---
+++ src/main/java/com/example/cs4500_sp19_josies/tests/ServiceSearchTest.java
@@ -0,0 +1,17 @@
+package com.example.cs4500_sp19_josies.tests;
+
+import com.example.cs4500_sp19_josies.models.Service;
+import com.example.cs4500_sp19_josies.models.ServiceQuestion;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+public class ServiceSearchTest {
+
+    @BeforeEach
+    void setUp() {
+
+
+    }
+}
