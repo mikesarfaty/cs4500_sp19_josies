f3971b9997066f07ded99ab9b402ba3ce46e7074
Khyati Singh <singh.khy@husky.neu.edu>
2019-02-23 12:00:02 -0500

    unit tests for service search results

---
+++ src/main/java/com/example/cs4500_sp19_josies/tests/ServiceSearchResultTest.java
@@ -0,0 +1,19 @@
+package com.example.cs4500_sp19_josies.tests;
+
+import com.example.cs4500_sp19_josies.models.Service;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ServiceSearchResultTest {
+
+  @BeforeEach
+  void setup() {
+
+    List<Service> searchResults = new ArrayList<>();
+
+    Service
+  }
+
+
+}
