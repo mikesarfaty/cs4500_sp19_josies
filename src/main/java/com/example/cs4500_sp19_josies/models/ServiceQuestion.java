860224ffe6b61394433f29c9b57d4823cde805f6
Nii Ako Boi-Doku <niiakoboi-doku@Niis-MBP.cable.rcn.com>
2019-02-25 23:07:48 -0500

    Created constructors for testing ServiceQuestion and ServiceAnswers

--- src/main/java/com/example/cs4500_sp19_josies/models/ServiceQuestion.java
+++ src/main/java/com/example/cs4500_sp19_josies/models/ServiceQuestion.java
@@ -10,38 +10,62 @@ import javax.persistence.Table;
 import java.util.List;
 
 @Entity
-@Table(name="service_questions")
+@Table(name = "service_questions")
 public class ServiceQuestion {
-  @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
-  private Integer id;
-  private String question;
-  private QuestionType type;
-  private String choices;
-  @OneToMany(mappedBy="serviceQuestion")
-  private List<ServiceAnswer> serviceAnswers;
-  public Integer getId() {
-    return id;
-  }
-  public void setId(Integer id) {
-    this.id = id;
-  }
-  public String getQuestion() {
-    return question;
-  }
-  public void setQuestion(String question) {
-    this.question = question;
-  }
-  public QuestionType getType() {
-    return type;
-  }
-  public void setType(QuestionType type) {
-    this.type = type;
-  }
-  public String getChoices() {
-    return choices;
-  }
-  public void setChoices(String choices) {
-    this.choices = choices;
-  }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String question;
+    private QuestionType type;
+    private String choices;
+    @OneToMany(mappedBy = "serviceQuestion")
+    private List<ServiceAnswer> serviceAnswers;
+
+
+    public ServiceQuestion(String question, QuestionType type,
+                           String choices, List<ServiceAnswer> serviceAnswers) {
+        this.question = question;
+        this.type = type;
+        this.choices = choices;
+        this.serviceAnswers = serviceAnswers;
+    }
+
+
+    public Integer getId() {
+
+        return id;
+    }
+
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+
+    public String getQuestion() {
+
+        return question;
+    }
+
+    public void setQuestion(String question) {
+
+        this.question = question;
+    }
+
+    public QuestionType getType() {
+
+        return type;
+    }
+
+
+    public void setType(QuestionType type) {
+        this.type = type;
+    }
+
+    public String getChoices() {
+        return choices;
+    }
+
+    public void setChoices(String choices) {
+        this.choices = choices;
+    }
 }
