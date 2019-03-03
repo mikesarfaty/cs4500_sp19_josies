a0ff41b234ca8698a831afd454ea92f2819ad9a1
Nii Ako Boi-Doku <niiakoboi-doku@Niis-MBP.fios-router.home>
2019-03-03 00:20:04 -0500

    Test true/false questions search works

--- src/main/java/com/example/cs4500_sp19_josies/tests/ServiceSearchTest.java
+++ src/main/java/com/example/cs4500_sp19_josies/tests/ServiceSearchTest.java
@@ -94,5 +94,63 @@ public class ServiceSearchTest {
         assertEquals(expectedResults, mySearch);
     }
 
+    @Test
+    void testServiceSearchTrueFalse() {
+
+        ServiceAnswer samsPets = new ServiceAnswer();
+        samsPets.setId(1);
+        samsPets.setTrueFalseAnswer(Boolean.TRUE);
+        samsPets.setServiceQuestion(havePets);
+        samsPets.setProvider(sam);
+        List<ServiceAnswer> samsAnswers = new ArrayList<>();
+        samsAnswers.add(samsPets);
+        sam.setServiceAnswers(samsAnswers);
+
+        ServiceAnswer jacksPets = new ServiceAnswer();
+        jacksPets.setId(2);
+        jacksPets.setTrueFalseAnswer(Boolean.FALSE);
+        jacksPets.setServiceQuestion(havePets);
+        jacksPets.setProvider(jack);
+        List<ServiceAnswer> jacksAnswers = new ArrayList<>();
+        jacksAnswers.add(jacksPets);
+        jack.setServiceAnswers(jacksAnswers);
+
+        ServiceAnswer josesPets = new ServiceAnswer();
+        josesPets.setId(3);
+        josesPets.setTrueFalseAnswer(Boolean.FALSE);
+        josesPets.setServiceQuestion(havePets);
+        josesPets.setProvider(jose);
+        List<ServiceAnswer> josesAnswers = new ArrayList<>();
+        josesAnswers.add(josesPets);
+        jose.setServiceAnswers(josesAnswers);
+
+        List<User> cleaners = new ArrayList<>();
+        cleaners.add(sam);
+        cleaners.add(jose);
+        cleaners.add(jack);
+        cleaning.setProviders(cleaners);
+
+        SearchPredicate predicate = new SearchPredicate();
+        predicate.setQuestion(havePets);
+        ServiceAnswer predAnswer = new ServiceAnswer();
+        predAnswer.setServiceQuestion(numberOfRooms);
+        predAnswer.setTrueFalseAnswer(Boolean.TRUE);
+        predicate.setAnswer(predAnswer);
+
+        SearchCriteria criteria = new SearchCriteria();
+
+        List<SearchPredicate> somePredicates = new ArrayList<>();
+        somePredicates.add(predicate);
+        criteria.setSearchPredicate(somePredicates);
+        ServiceSearch search = new ServiceSearch();
+        List<User> mySearch = search.searchForProviders(cleaning, criteria);
+
+        List<User> expectedResults = new ArrayList<>();
+        expectedResults.add(sam);
+
+
+        assertEquals(expectedResults, mySearch);
+    }
+
 
 }
