8b7580cd73cb2de3accdc4ebc2a2d747dd519d07
Nii Ako Boi-Doku <niiakoboi-doku@Niis-MBP.fios-router.home>
2019-03-03 00:21:02 -0500

    Test that results are sorted based on number of score

--- src/main/java/com/example/cs4500_sp19_josies/tests/ServiceSearchTest.java
+++ src/main/java/com/example/cs4500_sp19_josies/tests/ServiceSearchTest.java
@@ -152,5 +152,101 @@ public class ServiceSearchTest {
         assertEquals(expectedResults, mySearch);
     }
 
+    @Test
+    void testSearchIsSorted() {
 
+        ServiceAnswer samsRooms = new ServiceAnswer();
+        samsRooms.setId(1);
+        samsRooms.setMinRangeAnswer(5);
+        samsRooms.setMaxRangeAnswer(6);
+        samsRooms.setProvider(sam);
+        samsRooms.setServiceQuestion(numberOfRooms);
+
+        ServiceAnswer samsPets = new ServiceAnswer();
+        samsPets.setId(2);
+        samsPets.setTrueFalseAnswer(Boolean.TRUE);
+        samsPets.setServiceQuestion(havePets);
+        samsPets.setProvider(sam);
+
+        List<ServiceAnswer> samsAnswers = new ArrayList<>();
+        samsAnswers.add(samsRooms);
+        samsAnswers.add(samsPets);
+        sam.setServiceAnswers(samsAnswers);
+
+
+        ServiceAnswer josesRooms = new ServiceAnswer();
+        josesRooms.setId(3);
+        josesRooms.setMinRangeAnswer(3);
+        josesRooms.setMaxRangeAnswer(4);
+        josesRooms.setProvider(jose);
+        josesRooms.setServiceQuestion(numberOfRooms);
+
+        ServiceAnswer josesPets = new ServiceAnswer();
+        josesPets.setId(4);
+        josesPets.setTrueFalseAnswer(Boolean.FALSE);
+        josesPets.setServiceQuestion(havePets);
+        josesPets.setProvider(jose);
+
+        List<ServiceAnswer> josesAnswers = new ArrayList<>();
+        josesAnswers.add(josesRooms);
+        josesAnswers.add(josesPets);
+        jose.setServiceAnswers(josesAnswers);
+
+
+        ServiceAnswer jacksRooms = new ServiceAnswer();
+        jacksRooms.setId(5);
+        jacksRooms.setMinRangeAnswer(1);
+        jacksRooms.setMaxRangeAnswer(2);
+        jacksRooms.setProvider(jack);
+        jacksRooms.setServiceQuestion(numberOfRooms);
+
+        ServiceAnswer jacksPets = new ServiceAnswer();
+        jacksPets.setId(6);
+        jacksPets.setTrueFalseAnswer(Boolean.FALSE);
+        jacksPets.setServiceQuestion(havePets);
+        jacksPets.setProvider(jack);
+
+        List<ServiceAnswer> jacksAnswers = new ArrayList<>();
+        jacksAnswers.add(jacksRooms);
+        jacksAnswers.add(jacksPets);
+        jack.setServiceAnswers(jacksAnswers);
+
+
+        List<User> cleaners = new ArrayList<>();
+        cleaners.add(sam);
+        cleaners.add(jose);
+        cleaners.add(jack);
+        cleaning.setProviders(cleaners);
+
+        SearchPredicate roomPredicate = new SearchPredicate();
+        roomPredicate.setQuestion(numberOfRooms);
+        ServiceAnswer roomAnswer = new ServiceAnswer();
+        roomAnswer.setServiceQuestion(numberOfRooms);
+        roomAnswer.setMinRangeAnswer(3);
+        roomAnswer.setMaxRangeAnswer(5);
+        roomPredicate.setAnswer(roomAnswer);
+
+        SearchPredicate petPredicate = new SearchPredicate();
+        petPredicate.setQuestion(havePets);
+        ServiceAnswer petAnswer = new ServiceAnswer();
+        petAnswer.setServiceQuestion(havePets);
+        petAnswer.setTrueFalseAnswer(Boolean.TRUE);
+        petPredicate.setAnswer(petAnswer);
+
+        SearchCriteria criteria = new SearchCriteria();
+
+        List<SearchPredicate> somePredicates = new ArrayList<>();
+        somePredicates.add(roomPredicate);
+        somePredicates.add(petPredicate);
+        criteria.setSearchPredicate(somePredicates);
+        ServiceSearch search = new ServiceSearch();
+        List<User> mySearch = search.searchForProviders(cleaning, criteria);
+
+        List<User> expectedResults = new ArrayList<>();
+        expectedResults.add(sam);
+        expectedResults.add(jose);
+
+        assertEquals(expectedResults, mySearch);
+
+    }
 }
