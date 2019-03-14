f7c792c73e155f4d6621d8b91f80f6d2dcebca91
Drew McCann <mccann.an@husky.neu.edu>
2019-03-14 14:28:59 -0400

    use explicit imports rather than all imports

--- src/main/java/com/example/cs4500_sp19_josies/services/ServiceService.java
+++ src/main/java/com/example/cs4500_sp19_josies/services/ServiceService.java
@@ -5,7 +5,14 @@ import java.util.List;
 import com.example.cs4500_sp19_josies.models.Service;
 import com.example.cs4500_sp19_josies.repositories.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RestController
