170a1e7c842ed016ffb9438ff6e50fee0ea90e85
Drew McCann <mccann.an@husky.neu.edu>
2019-03-14 14:17:15 -0400

    allow front-end api access by adding cross origin annotation

--- src/main/java/com/example/cs4500_sp19_josies/services/ServiceService.java
+++ src/main/java/com/example/cs4500_sp19_josies/services/ServiceService.java
@@ -5,16 +5,11 @@ import java.util.List;
 import com.example.cs4500_sp19_josies.models.Service;
 import com.example.cs4500_sp19_josies.repositories.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin(origins="*")
 public class ServiceService {
     @Autowired
     ServiceRepository serviceRepository;
