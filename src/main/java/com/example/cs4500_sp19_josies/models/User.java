7bb5b3dac02f221ccf6b305d5b4095594e8167f3
jeffchampion <jeffrey.champion23@gmail.com>
2019-03-15 17:53:34 -0400

    user table is named users (#41)

--- src/main/java/com/example/cs4500_sp19_josies/models/User.java
+++ src/main/java/com/example/cs4500_sp19_josies/models/User.java
@@ -2,7 +2,13 @@ package com.example.cs4500_sp19_josies.models;
 
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="users")
