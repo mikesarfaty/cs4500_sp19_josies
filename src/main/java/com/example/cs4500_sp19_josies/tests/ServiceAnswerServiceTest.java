728c5f17c83887400e92edbd9b514af73581f629
Nii Ako Boi-Doku <niiakoboi-doku@Niis-MacBook-Pro.local>
2019-03-15 23:18:08 -0400

    Add Test class for the Service Answer Service

---
+++ src/main/java/com/example/cs4500_sp19_josies/tests/ServiceAnswerServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.cs4500_sp19_josies.tests;
+
+
+
+import com.example.cs4500_sp19_josies.services.ServiceAnswerService;
+
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ServiceAnswerService.class)
+public class ServiceAnswerServiceTest {
+
+
+
+}
