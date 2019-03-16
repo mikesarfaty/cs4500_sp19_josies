64c959f0d8417afdb36113ae8e17ea0add211a4f
Mike Sarfaty <32172768+mikesarfaty@users.noreply.github.com>
2019-03-15 23:16:37 -0400

    ServiceService CRUD tests (#51)
    * Service API Updates (#45)
    
    * init
    
    * use other server
    
    * CRUD tests for user details (#48)
    
    * added endpoint for finding via username
    
    * fixed api endpoint ambiguity
    
    * added tests for CRUD operations on user
    
    * init

--- src/main/resources/sql/create_services_table.sql
+++ src/main/resources/sql/create_services_table.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS services;
 CREATE TABLE services (
   id int(11) NOT NULL AUTO_INCREMENT,
-  service_name varchar(255) DEFAULT NULL,
+  title varchar(255) DEFAULT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
