514c5d8f8d7db5077a932c7bb8bc85438c5bd371
Nii Ako Boi-Doku <niiakoboi-doku@Niis-MacBook-Pro.local>
2019-03-16 05:08:50 -0400

    Putting better values into db with existing user ids

--- src/main/resources/sql/insert_service_answers.sql
+++ src/main/resources/sql/insert_service_answers.sql
@@ -1,4 +1,4 @@
-INSERT INTO service_answers (id, provider_id, service_question_id, true_false_answer) VALUES (1, 123, 1, 1);
-INSERT INTO service_answers (id, provider_id, service_question_id, true_false_answer) VALUES (2, 234, 1, 0);
-INSERT INTO service_answers (id, provider_id, service_question_id, true_false_answer) VALUES (3, 345, 1, 1);
+INSERT INTO service_answers (id, provider_id, service_question_id, true_false_answer) VALUES (1, 3, 1, 1);
+INSERT INTO service_answers (id, provider_id, service_question_id, true_false_answer) VALUES (2, 3, 1, 0);
+INSERT INTO service_answers (id, provider_id, service_question_id, true_false_answer) VALUES (3, 3, 1, 1);
 
