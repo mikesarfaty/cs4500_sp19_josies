a44eb9083e2617d206f304b18cf6d06da82c35b8
Khyati Singh <singh.khy@husky.neu.edu>
2019-04-04 13:41:28 -0400

    setters getters for zip

--- src/main/java/com/example/cs4500_sp19_josies/models/ServiceCategory.java
+++ src/main/java/com/example/cs4500_sp19_josies/models/ServiceCategory.java
@@ -30,7 +30,7 @@ public class ServiceCategory {
     public Integer getId() {
         return id;
     }
-    public void setgId(Integer id) {
+    public void setId(Integer id) {
         this.id = id;
     }
     public String getTitle() {
