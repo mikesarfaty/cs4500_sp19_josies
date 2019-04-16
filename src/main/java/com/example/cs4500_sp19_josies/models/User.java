86ee96bf93e489c246af659dacec132159a7faf2
Drew McCann <mccann.an@husky.neu.edu>
2019-04-16 00:01:02 -0400

    update users to have emails

--- src/main/java/com/example/cs4500_sp19_josies/models/User.java
+++ src/main/java/com/example/cs4500_sp19_josies/models/User.java
@@ -24,6 +24,7 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+    private String email;
     private String role;
 
     @OneToMany(mappedBy="provider")
@@ -53,13 +54,14 @@ public class User {
     public User() {
     }
 
-    public User(Integer id, String username, String password, String firstName, String lastName, String role) {
+    public User(Integer id, String username, String password, String firstName, String lastName, String email, String role) {
         super();
         this.id = id;
         this.username = username;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
         this.role = role;
     }
 
@@ -117,5 +119,13 @@ public class User {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
