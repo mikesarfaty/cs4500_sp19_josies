15efc1a0215fdc1336bfd48b5e93beeb7047a1ef
Drew McCann <mccann.an@husky.neu.edu>
2019-04-22 17:21:55 -0400

    update json field

--- src/test/java/com/example/cs4500_sp19_josies/ServiceAnswerServiceTest.java
+++ src/test/java/com/example/cs4500_sp19_josies/ServiceAnswerServiceTest.java
@@ -92,7 +92,7 @@ public class ServiceAnswerServiceTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value("1"))
-                .andExpect(jsonPath("$.trueFalseAnswer").value("true"));
+                .andExpect(jsonPath("$.answer").value("true"));
 
     }
 
